06c70199a3d15ae4252f627a3b3c53abc4abbaa6
Lucas Teixeira Pinto <85134699+Lucastepp@users.noreply.github.com>
2021-06-27 20:38:15 +0100

    Add files via upload

---
+++ 1060 Positive Numbers URI JUDGE PYTHON.py
@@ -0,0 +1,22 @@
+a = float(input())
+b = float(input())
+c = float(input())
+d = float(input())
+e = float(input())
+f = float(input())
+total = 0
+
+if (a > 0):
+    total += 1
+if (b > 0):
+    total += 1
+if (c > 0):
+    total += 1
+if (d > 0):
+    total += 1
+if (e > 0):
+    total += 1
+if (f > 0):
+    total += 1
+
+print(total, "valores positivos")
